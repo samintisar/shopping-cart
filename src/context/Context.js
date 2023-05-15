4d2e6d20670c37ed9f4a09a117c1ecefb9352e95
samintisar <samintisaruni@gmail.com>
2023-05-15 21:49:42 +0600

    used faker to generate product data

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,19 +1,21 @@
 import { createContext } from 'react'
-import faker from 'faker'
+import { faker } from '@faker-js/faker'
 
 const Cart = createContext();
 
 const Context = ({ children }) => {
 
     const products = [...Array(20)].map(() => ({
-        id: faker.datatype.uuid(),
+        id: faker.string.uuid(),
         name: faker.commerce.productName(),
         price: faker.commerce.price(),
-        image: faker.random.image(),
-        inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
+        image: faker.image.urlLoremFlickr(),
+        inStock: faker.number.float({ min: 1, max: 10, precision: 0.001 }),
         fastDelivery: faker.datatype.boolean(),
-        ratings: faker.random.arrayElement([1, 2, 3, 4, 5])
+        ratings: faker.number.int({ min: 1, max: 5 })
     }))
+
+    console.log(products);
 
     return (
         <Cart.Provider>{children}</Cart.Provider>
